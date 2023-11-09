75964d881727d3ac697c3307121e585593bcc2ca
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2023-11-09 19:22:05 -0300

    first

---
+++ src/main/java/com/example/crud/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.crud.repositories;
+
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+}
